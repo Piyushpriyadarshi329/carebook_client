f46f07046af2f8392985c4790dbf488550656ee9
Punysahlok Panda <link2psp@gmail.com>
2023-09-23 20:31:03 +0530

    live bookings

--- src/auth/useNotificationHandler.ts
+++ src/auth/useNotificationHandler.ts
@@ -7,7 +7,11 @@ export const useNotificationHandler = () => {
     console.log('data', data);
     switch (data?.name) {
       case CB_NOTIFICATION.NEW_BOOKING:
-        qc.removeQueries(['APPOINTMENTS', data.doctorId, Number(data.date)]);
+        qc.invalidateQueries([
+          'APPOINTMENTS',
+          data.doctorId,
+          Number(data.date),
+        ]);
         return;
     }
   };
